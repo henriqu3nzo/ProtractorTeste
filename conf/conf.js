0fbca1b62edbd77eb2a723f0d494cdf85bf8bbff
henriqu3nzo <ehsa2210@gmail.com>
2020-12-09 14:26:46 -0400

    Screenshot e Report adicionados

--- conf/conf.js
+++ conf/conf.js
@@ -12,7 +12,7 @@ exports.config = {
   
     // Spec patterns are relative to the current working directory when
     // protractor is called.
-    specs: ['calculator.js'],
+    specs: ['../tests/calculator.js'],
   
     // Options to be passed to Jasmine.
     jasmineNodeOpts: {
@@ -77,6 +77,6 @@ exports.config = {
         new HTMLReport().from('xmlresults.xml', testConfig);
         });
     }
-
+    
   };
   
